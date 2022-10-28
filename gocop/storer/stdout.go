1360c987a972d1021b7139638cb5ecc4bd175139
Kamal Nasser <kamal@digitalocean.com>
2022-10-28 21:42:30 +0300

    styling updates

--- gocop/storer/stdout.go
+++ gocop/storer/stdout.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sort"
 	"time"
 
 	"github.com/fatih/color"
 	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/muesli/reflow/indent"
 )
 
@@ -25,6 +27,9 @@ func (s *Stdout) InsertRun(ctx context.Context, run TestRun) error {
 	t.SetTitle("Test Run")
 	t.AppendHeader(table.Row{
 		"Job", "Repo", "Duration", "Flags",
+	})
+	t.SetColumnConfigs([]table.ColumnConfig{
+		{Number: 3, Align: text.AlignRight},
 	})
 	var flags []string
 	if run.Benchmark {
@@ -53,36 +58,61 @@ func (s *Stdout) GetRun(ctx context.Context, buildID int64) (*TestRun, error) {
 }
 
 func (s *Stdout) InsertTests(ctx context.Context, testResults []TestResult) error {
+	var (
+		byPackage = map[string][]TestResult{}
+		packages  []string
+	)
+
+	for _, r := range testResults {
+		if _, ok := byPackage[r.Package]; !ok {
+			byPackage[r.Package] = []TestResult{}
+			packages = append(packages, r.Package)
+		}
+		byPackage[r.Package] = append(byPackage[r.Package], r)
+	}
+	sort.Strings(packages)
+
 	t := table.NewWriter()
 	t.SetOutputMirror(indent.NewWriterPipe(os.Stdout, 2, nil))
 	t.SetStyle(table.StyleColoredDark)
+	t.Style().Box.MiddleHorizontal = "─"
 	t.SetTitle("Test Results")
 	t.AppendHeader(table.Row{
 		"Package", "Test", "Result", "Duration", "Coverage",
 	})
 	t.SetColumnConfigs([]table.ColumnConfig{
 		{Number: 1, AutoMerge: true},
+		{Number: 3, Align: text.AlignCenter},
+		{Number: 4, Align: text.AlignRight},
+		{Number: 5, Align: text.AlignRight},
 	})
 
-	for _, r := range testResults {
-		var resultColor func(string, ...interface{}) string
-		switch r.Result {
-		case "pass":
-			resultColor = color.GreenString
-		case "fail":
-			resultColor = color.RedString
-		case "skip":
-			resultColor = color.YellowString
-		}
-		result := resultColor(r.Result)
+	for _, p := range packages {
+		for _, r := range byPackage[p] {
+			if r.Test == "" {
+				r.Test = color.New(color.Italic, color.FgCyan).Sprint("<package results>")
+			}
 
-		t.AppendRow(table.Row{
-			r.Package,
-			r.Test,
-			result,
-			r.Duration.String(),
-			fmt.Sprintf("%1.2f%%", r.Coverage/100),
-		})
+			var resultColor func(string, ...interface{}) string
+			switch r.Result {
+			case "pass":
+				resultColor = color.GreenString
+			case "fail":
+				resultColor = color.RedString
+			case "skip":
+				resultColor = color.YellowString
+			}
+			result := resultColor(r.Result)
+
+			t.AppendRow(table.Row{
+				r.Package,
+				r.Test,
+				result,
+				r.Duration.String(),
+				fmt.Sprintf("%1.2f%%", r.Coverage/100),
+			})
+		}
+		t.AppendSeparator()
 	}
 
 	t.Render()
